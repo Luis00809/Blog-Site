58dc969b1b402e3dd9c2bd7d4bf8257a64fc2367
Luis00809 <davidcarvajal008@gmail.com>
2023-09-16 00:26:59 -0700

    creates routes for user and homepage

---
+++ controllers/home-routes.js
@@ -0,0 +1,34 @@
+const router = require('express').Router();
+const Blog = require('../models/Blog');
+
+// will display the homepage with all exitisting blog posts if there are any created
+// gets all blogs
+router.get('/', async (req, res) => {
+    const blogData = await Blog.findAll().catch((err) => {
+        res.json(err);
+    });
+
+    const blogs = blogData.map((blog) => blog.get({ plain: true }));
+    res.status(200).json(blogs);
+    // will need to change to display handlebars later 
+    // for now using for making sure routes work
+});
+
+router.get('/blog/:id', async (req, res) => {
+    try {
+        const blogData = await Blog.findByPk(req.params.id);
+        if (!blogData){
+            res.status(404).json({message: "That blog doesn't exist!"});
+            return;
+        }
+
+        const blog = blogData.get({ plain: true });
+        res.status(200).json(blog);
+
+        // also need to change this route to a render method 
+        // will insomnia work with this set up????
+
+    } catch (err) {
+        res.status(500).json(err)
+    }
+})
