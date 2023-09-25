92ff32fd850c9930aa4446a9056bfcc1a671369f
Luis00809 <davidcarvajal008@gmail.com>
2023-09-25 11:01:03 -0700

    url

--- config/connections.js
+++ config/connections.js
@@ -5,6 +5,7 @@ const sequelize = new Sequelize(
   process.env.DB_NAME,
   process.env.DB_USER,
   process.env.DB_PASSWORD,
+  process.env.DATABASE_URL,
   {
     host: 'localhost',
     dialect: 'mysql',
